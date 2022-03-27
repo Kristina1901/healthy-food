65a7b2354c7a1bd8c68206f96b00cdf5deb1cc61
Kristina <ktuzova1901@gmail.com>
2022-03-27 16:21:20 +0300

    finished styles

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss';
+new Swiper('.swiper',{
+                navigation: {
+                        nextEl: '.swiper-button-prev',
+                        prevEl: '.swiper-button-next',
+                },
+            });
